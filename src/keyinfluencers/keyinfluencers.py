c6c13235b851b23e098fe9c81844ab935667bf84
davide-andreoli <davide.andreoli@icloud.com>
2025-04-19 07:55:20 +0200

    Feat: class init and fit

---
+++ src/keyinfluencers/keyinfluencers.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+import shap
+
+class KeyInfluencers():
+
+    def __init__(self, dataframe: pd.DataFrame, target: str):
+
+        self.dataframe = dataframe
+        self.target = target
+
+        self.model_pipeline = None
+        self.explainer = None
+        self.feature_names = None
+        self.shap_values = None
+    
+    def fit(self):
+        
+        X = self.dataframe.drop(self.target, axis=1)
+        y = self.dataframe[self.target]
+
+        categorical_columns = [column for column in X.columns if self._determine_column_type(X[column]) == 'categorical']
+        time_columns = [column for column in X.columns if self._determine_column_type(X[column]) == 'time']
+        numerical_columns = [column for column in X.columns if self._determine_column_type(X[column]) == 'numerical']
+
+        categorical_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        numerical_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('scaler', StandardScaler())
+        ])
+
+        #TODO: Add preprocessor for time data
+
+        preprocessor = ColumnTransformer([
+            ('categorical', categorical_pipeline, categorical_columns),
+            ('numerical', numerical_pipeline, numerical_columns)
+        ])
+
+        target_type = self._determine_column_type(y)
+
+        # X_train, X_test, y_train, y_test = train_test_split(X, y)
+        # TODO: Add automatic model choice based on performance
+        if target_type == 'categorical':
+            predictor = LogisticRegression(solver='lbfgs', max_iter=1000)
+        else:
+            predictor = LinearRegression()
+
+        self.model_pipeline = Pipeline([
+            ('preprocessor', preprocessor),
+            ('predictor', predictor)
+        ])
+
+        self.model_pipeline.fit(X, y)
+
+        self.feature_names = self.model_pipeline.named_steps['preprocessor'].get_feature_names_out()
+        self.explainer = shap.Explainer(self.model_pipeline.named_steps['predictor'], self.model_pipeline.named_steps['preprocessor'].transform(X))
+        self.shap_values = self.explainer(self.model_pipeline.named_steps['preprocessor'].transform(X))
+        # Use shap to extract feature importance
+
+    def _determine_column_type(self, column: pd.Series):
+        #TODO: make this an enum
+        if column.dtype in ['object', 'category', 'bool'] or len(column.unique()) <= 10:
+            return 'categorical'
+        elif column.dtype == 'datetime64' :
+            return 'time'
+        else:
+            return 'numerical'
